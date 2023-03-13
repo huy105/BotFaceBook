418679b81db64b9dba951f3c4ec328ac66ed3de4
Huy105 <luonggiahuy105@gmail.com>
2023-03-13 21:33:22 +0700

    adding share_status

--- action.py
+++ action.py
@@ -27,6 +27,7 @@ class BotFaceBook():
         self.options.add_argument('--profile-directory=' + profile)
         self.ser = Service("E:/Data/AI_Files/KLTN/envDriver/chromedriver.exe")
         self.driver = webdriver.Chrome(service= self.ser, chrome_options= self.options)
+        self.driver.implicitly_wait(5)
 
     def get_fb(self, url: str = 'https://vi-vn.facebook.com/'):
         self.driver.get(url)
@@ -39,9 +40,6 @@ class BotFaceBook():
         return user_name, user_pass
 
     def sign_in_fb(self, user_name: str, user_pass: str):
-
-        self.driver.implicitly_wait(5)
-
         user_box = self.driver.find_element(by=By.NAME, value="email")
         pass_box = self.driver.find_element(by=By.NAME, value="pass")
         login_button = self.driver.find_element(by=By.NAME, value="login")
@@ -57,13 +55,9 @@ class BotFaceBook():
         status_box_button = self.driver.find_element(by=By.XPATH, value=valueXPath)                         
         status_box_button.click()
 
-        self.driver.implicitly_wait(5)
-
         valueXPath = '//div[@class="xzsf02u x1a2a7pz x1n2onr6 x14wi4xw x9f619 x1lliihq x5yr21d xh8yej3 notranslate"]'
         type_status = self.driver.find_element(by=By.XPATH, value=valueXPath) 
         type_status.send_keys(status_str)
-
-        self.driver.implicitly_wait(5)
 
         valueXPath = '//div[@aria-label="Đăng" and @role="button"]'
         status_post_button = self.driver.find_element(by=By.XPATH, value=valueXPath)
@@ -145,7 +139,6 @@ class BotFaceBook():
         xpath_stt2 = "//div[@class='xzsf02u xngnso2 xo1l8bm x1qb5hxa']"
 
         self.driver.get(profile_url)
-        self.driver.implicitly_wait(5)
     
         list_ele_stt1 = self.driver.find_elements(By.XPATH, xpath_stt1)
         list_ele_stt2 = self.driver.find_elements(By.XPATH, xpath_stt2)
@@ -157,8 +150,7 @@ class BotFaceBook():
         # get element untill enough
         while num_post_get < num_status:
             ActionChains(self.driver).scroll_by_amount(0, amount_scroll).perform()
-            self.driver.implicitly_wait(5)
-            
+            time.sleep(2)
             list_ele_stt1 = self.driver.find_elements(By.XPATH, xpath_stt1)
             list_ele_stt2 = self.driver.find_elements(By.XPATH, xpath_stt2)
             num_post_get = len(list_ele_stt1) + len(list_ele_stt2) 
@@ -213,19 +205,17 @@ class BotFaceBook():
             f.write(str(id_data))  
             f.close()
 
-    def react_status(self, emotion: int, num_status: int = 1):
+    def react_status(self, emotion: int):
         """
         emotion: is the kind of emotion we want to react
         (0: Thích, 1: Yêu Thích, 2: Thương Thương, 3: Haha, 4: Wow, 5: Buồn, 6: Phẫn nộ) 
         num_status: number of status,.. you want react
         """
-        self.driver.implicitly_wait(5)
         xpath_react = "//div[@aria-label='{}' and @role='button']".format(self.list_react[emotion])
-        xpath_hover = "//div[@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x3nfvp2 x1q0g3np x87ps6o x1lku1pv x1a2a7pz x5ve5x3"
+        xpath_hover = "//div[@class='x1i10hfl x1qjc9v5 xjbqb8w xjqpnuy xa49m3k xqeqjp1 x2hbi6w x13fuv20 xu3j5b3 x1q0q8m5 x26u7qi x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xdl72j9 x2lah0s xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x2lwn1j xeuugli xexx8yu x4uap5 x18d9i69 xkhd6sd x1n2onr6 x16tdsg8 x1hl2dhg x1ja2u2z x1t137rt x1o1ewxj x3x9cwd x1e5q0jg x13rtm0m x3nfvp2 x1q0g3np x87ps6o x1lku1pv x1a2a7pz x5ve5x3']"
         
         hover_e_react = self.driver.find_element(By.XPATH, xpath_hover)
         ActionChains(self.driver).move_to_element(hover_e_react).perform()
-        self.driver.implicitly_wait(5)
         try:
             element = self.driver.find_element(By.XPATH, xpath_react)
             if element.is_displayed():
@@ -235,4 +225,25 @@ class BotFaceBook():
         except:
             print("Already or cannot find")
         
-        time.sleep(5)
+    def share_status(self, caption: str = '', action: int = 0):
+        """
+        caption: is caption you wanna share
+        action: is kind of sharing you wanna choose (from 0 to 6)
+        """
+        xpath_share = "//div[@aria-label='Gửi nội dung này đến bạn bè hoặc đăng trên dòng thời gian của bạn.']"
+        list_xpath_share = "//span[@class='x193iq5w xeuugli x13faqbe x1vvkbs x1xmvt09 x1lliihq x1s928wv xhkezso x1gmr53x x1cpjm7i x1fgarty x1943h6x xudqn12 x3x7a5m x6prxxf xvq8zen xk50ysn xzsf02u x1yc453h']"
+        xpath_share1 = "//p[@class='xdj266r x11i5rnm xat24cr x1mh8g0r x16tdsg8']"
+        xpath_share1_button = "//div[@aria-label='Chia sẻ']"
+
+        e_share = self.driver.find_element(By.XPATH, xpath_share)
+        e_share.click()
+        time.sleep(2)
+        list_share = self.driver.find_elements(By.XPATH, list_xpath_share)
+        list_share[action].click()
+        
+        # take the action (if action = 1 we can use caption)
+        if action == 1:
+            self.driver.find_element(By.XPATH, xpath_share1).send_keys(caption)
+            self.driver.find_element(By.XPATH, xpath_share1_button).click()
+
+
